58719a31e31eb6ec5c55b6cbba2e2f27e428145d
ridho20xpplg2 <ridho14371@gmail.com>
2024-12-01 20:35:50 +0700

    day14

---
+++ solveabstrackclasses.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+abstract class Book
+{
+    
+    protected String title;
+    protected  String author;
+    
+    public Book(String t,String a){
+        title=t;
+        author=a;
+    }
+    public abstract void display();
+
+
+}
+
+//Write MyBook class
+// Tambahkan kelas MyBook
+class MyBook : Book
+{
+    private int price;
+
+    public MyBook(string t, string a, int p) : base(t, a)
+    {
+        price = p;
+    }
+
+    public override void display()
+    {
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Author: " + author);
+        Console.WriteLine("Price: " + price);
+    }
+}
+
+class Solution {
+    static void Main(String[] args) {
+      String title=Console.ReadLine();
+      String author=Console.ReadLine();
+      int price=Int32.Parse(Console.ReadLine());
+      Book new_novel=new MyBook(title,author,price);
+      new_novel.display();
+    }
+}
