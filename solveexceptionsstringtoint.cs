a03d088c20688568738f1446ec61c13be69a435e
ridho20xpplg2 <ridho14371@gmail.com>
2024-12-04 20:41:14 +0700

    day17

---
+++ solveexceptionsstringtoint.cs
@@ -0,0 +1,32 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+        try
+        {
+            int result = int.Parse(S);
+            Console.WriteLine(result);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
